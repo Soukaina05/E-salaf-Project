4ff4834fa1ae5870b38642dad7c841b7f2875513
Soukaina Zahti <114617766+Soukaina05@users.noreply.github.com>
2023-04-03 16:38:43 +0000

    Update credit.java

--- model/credit.java
+++ model/credit.java
@@ -1,18 +1,19 @@
 package com.exemple.model;
-import java.util.Date;
+
+import java.sql.Timestamp;
 
 
 public class credit {
         private long credit_id;
         private int quantity;
-       private Date credit_date;
+       private Timestamp credit_date;
        private long client_id;
         private long produit_id;
 
-    public credit(long credit_id, int quantity,  long client_id, long produit_id) {
+    public credit(long credit_id, int quantity, Timestamp credit_date, long client_id, long produit_id) {
         this.credit_id = credit_id;
         this.quantity = quantity;
-
+        this.credit_date = credit_date;
         this.client_id = client_id;
         this.produit_id = produit_id;
     }
@@ -44,11 +45,11 @@ public class credit {
 
 
 
-    public Date getCredit_date() {
+    public Timestamp getCredit_date() {
         return credit_date;
     }
 
-    public void setCredit_date(Date credit_date) {
+    public void setCredit_date(Timestamp credit_date) {
         this.credit_date = credit_date;
     }
 
